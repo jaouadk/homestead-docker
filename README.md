1d6fff4a3b138cca212e15256c7d4cd43715d2dd
Jaouad E <jaouad.elmoussaoui@gmail.com>
2016-04-25 12:29:24 +0100

    Working php7.0 image

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Create a homestead docker container for your development env. ( files taken from
 ### Install docker && docker compose
 please refer to these tutorials:
 * install docker (https://docs.docker.com/installation/ubuntulinux/)
-```shell 
+```shell
 curl -sSL https://get.docker.com/ | sh
 ```
 * install docker compose (https://docs.docker.com/compose/install/)
@@ -12,19 +12,21 @@ curl -sSL https://get.docker.com/ | sh
 curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 ```
 
-### Build the homestead image
+### Pull homestead image
 ```shell
-git clone https://github.com/shincoder/homestead-docker.git
-cd homestead-docker
-docker build -t shincoder/homestead .
+docker pull shincoder/homestead:php7.0
 ```
 
-### Launch your containers
+### Clone && Edit docker-compose.yml
+```shell
+git clone https://github.com/shincoder/homestead-docker.git
+```
+
+### Start your containers
 There are only two containers to run. web container ( includes everything except your database ), and mariadb container.
 ```shell
 sudo docker-compose up -d
 ```
-this will build the image, and start it.
 
 ### SSH into the container (password: secret):
 ```shell
@@ -41,14 +43,10 @@ In the host, update ``` /etc/hosts ``` to include your app domain:
 127.0.0.1               myapp.dev
 ```
 
-### Everything should work - enjoy !!!
-Our web container starts nginx, php5-fpm, redis, beanstalk. and has gruntjs, gulp, bower.
+### That's it
+Our web container starts nginx, php-fpm, redis, beanstalk. and has gruntjs, gulp, bower...etc
 some relevant ports have been added to docker-compose.yml ( livereload standard port, karma server port ), change them if you need to.
 
 ### Notes
 - Use docker's local IP address to connect to your database. Run `docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${CID}`, where `${CID}` is docker container ID of the database
 - Databases: by default mariadb is used as a database, but you are free to use any database you want: choose from these excellent images by Tutum: [tutum/mysql](https://github.com/tutumcloud/mysql) or [tutum/postgresql](https://github.com/tutumcloud/postgresql), they expose different environment variables, so don't forget to update your docker-compose file accordingly.
-
-
-### TODO
-- [x] use supervisor (should monitor: redis, nginx, php-fpm, beanstalk, ssh server)
--- provision.sh
+++ provision.sh
@@ -4,64 +4,51 @@
 # https://github.com/laravel/settler
 
 # Update Package List
-apt update
-apt upgrade -y
+apt-get update
+apt-get upgrade -y
 
 # Force Locale
 echo "LC_ALL=en_US.UTF-8" >> /etc/default/locale
 locale-gen en_US.UTF-8
-# fixes php 5.6 install through ondrej ppa
 export LANG=en_US.UTF-8
 
 # Install ssh server
-apt -y install openssh-server pwgen
+apt-get -y install openssh-server pwgen
 mkdir -p /var/run/sshd
 sed -i "s/UsePrivilegeSeparation.*/UsePrivilegeSeparation no/g" /etc/ssh/sshd_config
 sed -i "s/UsePAM.*/UsePAM no/g" /etc/ssh/sshd_config
 sed -i "s/PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config
 
-# Install Some PPAs
-apt install -y software-properties-common nano curl
-
-curl --silent --location https://deb.nodesource.com/setup_5.x | bash -
+# Basic packages
+apt-get install -y sudo software-properties-common nano curl \
+build-essential dos2unix gcc git git-flow libmcrypt4 libpcre3-dev apt-utils \
+make python2.7-dev python-pip re2c supervisor unattended-upgrades whois vim zip unzip
 
 # Update Package Lists
-apt update
+apt-get update
 
 # Create homestead user
 adduser homestead
 usermod -p $(echo secret | openssl passwd -1 -stdin) homestead
-
 # Add homestead to the sudo group and www-data
 usermod -aG sudo homestead
 usermod -aG www-data homestead
 
-# Install Some Basic Packages
-apt install -y build-essential dos2unix gcc git git-flow libmcrypt4 libpcre3-dev \
-make python2.7-dev python-pip re2c supervisor unattended-upgrades whois vim
-
-# Install A Few Helpful Python Packages
-pip install httpie
-pip install fabric
-pip install python-simple-hipchat
-
-# Set My Timezone
+# Timezone
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
-# Install PHP Stuffs
-apt install -y php-cli php-dev php-pear \
+# PHP
+apt-get install -y php-cli php-dev php-pear \
 php-mysql php-pgsql php-sqlite3 \
 php-apcu php-json php-curl php-gd \
 php-gmp php-imap php-mcrypt php-xdebug \
-php-memcached php-redis
+php-memcached php-redis php-mbstring
 
-# Make MCrypt Available
-ln -s /etc/php/7.0/conf.d/mcrypt.ini /etc/php/7.0/mods-available
+# Nginx & PHP-FPM
+apt-get install -y nginx php-fpm
+
+# Enable mcrypt
 phpenmod mcrypt
-
-# Install Mailparse PECL Extension
-pecl install -Z mailparse
-echo "extension=mailparse.so" > /etc/php/7.0/mods-available/mailparse.ini
 
 # Install Composer
 curl -sS https://getcomposer.org/installer | php
@@ -70,8 +57,8 @@ mv composer.phar /usr/local/bin/composer
 # Add Composer Global Bin To Path
 printf "\nPATH=\"/home/homestead/.composer/vendor/bin:\$PATH\"\n" | tee -a /home/homestead/.profile
 
-# Install Laravel Envoy
-sudo su homestead <<'EOF'
+# Laravel Envoy
+su homestead <<'EOF'
 /usr/local/bin/composer global require "laravel/envoy=~1.0"
 EOF
 
@@ -80,15 +67,6 @@ sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.0/cli/php.in
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.0/cli/php.ini
 sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.0/cli/php.ini
 sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.0/cli/php.ini
-
-# Install Nginx & PHP-FPM
-apt install -y nginx php-fpm
-
-rm /etc/nginx/sites-enabled/default
-rm /etc/nginx/sites-available/default
-
-# Setup Some PHP-FPM Options
-ln -s /etc/php/7.0/mods-available/mailparse.ini /etc/php/7.0/fpm/conf.d/20-mailparse.ini
 
 sed -i "s/.*daemonize.*/daemonize = no/" /etc/php/7.0/fpm/php-fpm.conf
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.0/fpm/php.ini
@@ -101,11 +79,9 @@ sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.0/fpm/php.ini
 echo "xdebug.remote_enable = 1" >> /etc/php/7.0/fpm/conf.d/20-xdebug.ini
 echo "xdebug.remote_connect_back = 1" >> /etc/php/7.0/fpm/conf.d/20-xdebug.ini
 echo "xdebug.remote_port = 9000" >> /etc/php/7.0/fpm/conf.d/20-xdebug.ini
-
 echo "xdebug.var_display_max_depth = -1" >> /etc/php/7.0/fpm/conf.d/20-xdebug.ini
 echo "xdebug.var_display_max_children = -1" >> /etc/php/7.0/fpm/conf.d/20-xdebug.ini
 echo "xdebug.var_display_max_data = -1" >> /etc/php/7.0/fpm/conf.d/20-xdebug.ini
-
 echo "xdebug.max_nesting_level = 500" >> /etc/php/7.0/fpm/conf.d/20-xdebug.ini
 
 # Set The Nginx & PHP-FPM User
@@ -113,29 +89,33 @@ sed -i '1 idaemon off;' /etc/nginx/nginx.conf
 sed -i "s/user www-data;/user homestead;/" /etc/nginx/nginx.conf
 sed -i "s/# server_names_hash_bucket_size.*/server_names_hash_bucket_size 64;/" /etc/nginx/nginx.conf
 
+mkdir -p /run/php
+touch /run/php/php7.0-fpm.sock
 sed -i "s/user = www-data/user = homestead/" /etc/php/7.0/fpm/pool.d/www.conf
 sed -i "s/group = www-data/group = homestead/" /etc/php/7.0/fpm/pool.d/www.conf
-
 sed -i "s/;listen\.owner.*/listen.owner = homestead/" /etc/php/7.0/fpm/pool.d/www.conf
 sed -i "s/;listen\.group.*/listen.group = homestead/" /etc/php/7.0/fpm/pool.d/www.conf
 sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.0/fpm/pool.d/www.conf
 
 # Install Node
-apt install -y nodejs
+curl --silent --location https://deb.nodesource.com/setup_5.x | bash -
+apt-get install -y nodejs
 npm install -g grunt-cli
 npm install -g gulp
 npm install -g bower
 
 # Install SQLite
-apt install -y sqlite3 libsqlite3-dev
+apt-get install -y sqlite3 libsqlite3-dev
 
-# Install A Few Other Things
-apt install -y redis-server memcached beanstalkd
+# Memcached
+apt-get install -y memcached
 
-# Configure Beanstalkd
+# Beanstalkd
+apt-get install -y beanstalkd
 sed -i "s/#START=yes/START=yes/" /etc/default/beanstalkd
 
-# Configure Redis
+# Redis
+apt-get install -y redis-server
 sed -i "s/daemonize yes/daemonize no/" /etc/redis/redis.conf
 
 # Configure default nginx site
@@ -176,6 +156,9 @@ block="server {
     }
 }
 "
+
+rm /etc/nginx/sites-enabled/default
+rm /etc/nginx/sites-available/default
 
 cat > /etc/nginx/sites-enabled/default
 echo "$block" > "/etc/nginx/sites-enabled/default"
