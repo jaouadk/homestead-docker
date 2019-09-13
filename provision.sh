b4892a5b4e64f1d0f62f4c9ec0a68c869efe9fa5
el hamouss adil <el.hamouss.adil92@gmail.com>
2019-09-13 11:22:58 +0100

    upgrade to php7.3

--- provision.sh
+++ provision.sh
@@ -42,16 +42,16 @@ usermod -aG www-data homestead
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
 # PHP
-apt-get install -y php7.2-cli php7.2-dev \
-php7.2-mysql php7.2-pgsql php7.2-sqlite3 php7.2-soap \
-php7.2-json php7.2-curl php7.2-gd \
-php7.2-gmp php7.2-imap php-xdebug \
-php7.2-mbstring php7.2-zip \
+apt-get install -y php7.3-cli php7.3-dev \
+php7.3-mysql php7.3-pgsql php7.3-sqlite3 php7.3-soap \
+php7.3-json php7.3-curl php7.3-gd \
+php7.3-gmp php7.3-imap php-xdebug \
+php7.3-mbstring php7.3-zip \
 php-pear php-apcu php-memcached php-redis \
-php7.2-dom php7.2-bcmath
+php7.3-dom php7.3-bcmath
 
 # Nginx & PHP-FPM
-apt-get install -y nginx php7.2-fpm
+apt-get install -y nginx php7.3-fpm
 
 # Install Composer
 curl -sS https://getcomposer.org/installer | php
@@ -66,26 +66,26 @@ su homestead <<'EOF'
 EOF
 
 # Set Some PHP CLI Settings
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.2/cli/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.2/cli/php.ini
-sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.2/cli/php.ini
-sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.2/cli/php.ini
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.3/cli/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.3/cli/php.ini
+sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.3/cli/php.ini
+sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.3/cli/php.ini
 
-sed -i "s/.*daemonize.*/daemonize = no/" /etc/php/7.2/fpm/php-fpm.conf
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.2/fpm/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.2/fpm/php.ini
-sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.2/fpm/php.ini
-sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.2/fpm/php.ini
-sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.2/fpm/php.ini
+sed -i "s/.*daemonize.*/daemonize = no/" /etc/php/7.3/fpm/php-fpm.conf
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.3/fpm/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.3/fpm/php.ini
+sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.3/fpm/php.ini
+sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.3/fpm/php.ini
+sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.3/fpm/php.ini
 
 # Enable Remote xdebug
-echo "xdebug.remote_enable = 1" >> /etc/php/7.2/fpm/conf.d/20-xdebug.ini
-echo "xdebug.remote_connect_back = 1" >> /etc/php/7.2/fpm/conf.d/20-xdebug.ini
-echo "xdebug.remote_port = 9000" >> /etc/php/7.2/fpm/conf.d/20-xdebug.ini
-echo "xdebug.var_display_max_depth = -1" >> /etc/php/7.2/fpm/conf.d/20-xdebug.ini
-echo "xdebug.var_display_max_children = -1" >> /etc/php/7.2/fpm/conf.d/20-xdebug.ini
-echo "xdebug.var_display_max_data = -1" >> /etc/php/7.2/fpm/conf.d/20-xdebug.ini
-echo "xdebug.max_nesting_level = 500" >> /etc/php/7.2/fpm/conf.d/20-xdebug.ini
+echo "xdebug.remote_enable = 1" >> /etc/php/7.3/fpm/conf.d/20-xdebug.ini
+echo "xdebug.remote_connect_back = 1" >> /etc/php/7.3/fpm/conf.d/20-xdebug.ini
+echo "xdebug.remote_port = 9000" >> /etc/php/7.3/fpm/conf.d/20-xdebug.ini
+echo "xdebug.var_display_max_depth = -1" >> /etc/php/7.3/fpm/conf.d/20-xdebug.ini
+echo "xdebug.var_display_max_children = -1" >> /etc/php/7.3/fpm/conf.d/20-xdebug.ini
+echo "xdebug.var_display_max_data = -1" >> /etc/php/7.3/fpm/conf.d/20-xdebug.ini
+echo "xdebug.max_nesting_level = 500" >> /etc/php/7.3/fpm/conf.d/20-xdebug.ini
 
 # Not xdebug when on cli
 phpdismod -s cli xdebug
@@ -96,12 +96,12 @@ sed -i "s/user www-data;/user homestead;/" /etc/nginx/nginx.conf
 sed -i "s/# server_names_hash_bucket_size.*/server_names_hash_bucket_size 64;/" /etc/nginx/nginx.conf
 
 mkdir -p /run/php
-touch /run/php/php7.2-fpm.sock
-sed -i "s/user = www-data/user = homestead/" /etc/php/7.2/fpm/pool.d/www.conf
-sed -i "s/group = www-data/group = homestead/" /etc/php/7.2/fpm/pool.d/www.conf
-sed -i "s/;listen\.owner.*/listen.owner = homestead/" /etc/php/7.2/fpm/pool.d/www.conf
-sed -i "s/;listen\.group.*/listen.group = homestead/" /etc/php/7.2/fpm/pool.d/www.conf
-sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.2/fpm/pool.d/www.conf
+touch /run/php/php7.3-fpm.sock
+sed -i "s/user = www-data/user = homestead/" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/group = www-data/group = homestead/" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/;listen\.owner.*/listen.owner = homestead/" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/;listen\.group.*/listen.group = homestead/" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.3/fpm/pool.d/www.conf
 
 # Install Node
 curl --silent --location https://deb.nodesource.com/setup_8.x | bash -
@@ -152,7 +152,7 @@ block="server {
 
     location ~ \.php$ {
         fastcgi_split_path_info ^(.+\.php)(/.+)$;
-        fastcgi_pass unix:/run/php/php7.2-fpm.sock;
+        fastcgi_pass unix:/run/php/php7.3-fpm.sock;
         fastcgi_index index.php;
         include fastcgi.conf;
     }
